361207cb6fa5215d12b423c72065c059d430ed9a
Chris Malone <31215637+chris-ac-malone@users.noreply.github.com>
2018-07-06 14:29:27 -0600

    adjusting login route

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ express()
     var loginQuery = "";
     var results = "";
       db.query("SELECT id, username, password FROM users WHERE username = acedoutry", function(err, result) {
-        res.send("Test");
+        res.send(result);
       })
 
     
