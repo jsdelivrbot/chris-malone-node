b0f16a96ea2e50cb976210dfa69be8a7f8f47412
Chris Malone <31215637+chris-ac-malone@users.noreply.github.com>
2018-07-06 13:09:46 -0600

    adjusting login route

--- index.js
+++ index.js
@@ -205,8 +205,7 @@ express()
         for (var i = 0; i < result.rows.length; i++) {
           if(result.rows[i].username == username)
             dbPass = result.rows[i].password;
-              res.send(dbPass);
-
+          console.log(dbPass);
           else{}
           
         }//END FOR LOOP
@@ -220,7 +219,7 @@ express()
         }
     })//END QUERY
     //req.session.loggedIn = true;
-    // res.send(password + "\n" + dbPass);
+    res.send(password + "\n" + dbPass);
             //res.redirect('/readingguide');
   })
  
